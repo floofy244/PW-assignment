047f3a5f3ceb0ffa59683f047ff487bfca515bed
floofy244 <iamaviralmalik@gmail.com>
2024-05-16 01:16:06 +0530

    pushed code again without node modules

---
+++ src/public/js/search.js
@@ -0,0 +1,6 @@
+var searchbox = document.getElementById("home_search");
+var btn = document.getElementById("searchIcon");
+btn.addEventListener("click", function () {
+  localStorage.setItem("homesearch", JSON.stringify(searchbox.value));
+  window.location.href = "internships.ejs";
+});
